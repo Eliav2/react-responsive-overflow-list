a9df3277dea1634d0f187e7ecf7954990dcc90de
Eliav Louski <louski.a@gmail.com>
2025-09-22 02:56:02 +0300

    polish

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -3,13 +3,18 @@ import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "node:url";
 import vike from "vike/plugin";
 
-// import { ssr } from "vite-plugin-ssr/plugin";
-
 export default defineConfig(() => {
   const appMode = process.env.VITE_APP_MODE || ("spa" as "spa" | "ssr");
   console.log(`VITE_APP_MODE is "${appMode}"`);
 
-  const sharedConfig: Partial<import("vite").UserConfig> = {
+  return {
+    plugins: [react(), vike()],
+    ssr: {
+      // Don't externalize React for SSR
+      noExternal: ["react-responsive-overflow-list", "react-syntax-highlighter", "@types/react-syntax-highlighter"],
+      // Handle CommonJS modules
+      // external: ["react-copy-to-clipboard"],
+    },
     base: process.env.NODE_ENV === "production" ? "/react-responsive-overflow-list/" : "/",
     resolve: {
       // trick to avoid needing to build-watch the library when developing the demo
@@ -17,37 +22,5 @@ export default defineConfig(() => {
         "react-responsive-overflow-list": fileURLToPath(new URL("../src/index.ts", import.meta.url)),
       },
     },
-  };
-
-  // if (appMode === "spa") {
-  //   return {
-  //     plugins: [react()],
-  //     ...sharedConfig,
-  //   };
-  // }
-
-  // ssr mode
-  return {
-    plugins: [
-      // ssr({
-      //   prerender: true,
-      // }),
-      react(),
-      vike(),
-      // vitePrerenderPlugin({
-      //   renderTarget: "#root",
-      //   prerenderScript: path.resolve(__dirname, "src/prerender.tsx"),
-      // }),
-    ],
-    // build: {
-    //   minify: false,
-    // },
-    ssr: {
-      // Don't externalize React for SSR
-      noExternal: ["react-responsive-overflow-list", "react-syntax-highlighter", "@types/react-syntax-highlighter"],
-      // Handle CommonJS modules
-      // external: ["react-copy-to-clipboard"],
-    },
-    ...sharedConfig,
   };
 });
